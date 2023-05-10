0f7844ee1758f5f01f0e9cd3be7917ab6dbcf76c
banana <banana99ou@gmail.com>
2023-05-11 08:44:20 +0900

    switched to vscode git

--- mechanum.ino
+++ mechanum.ino
@@ -2,66 +2,101 @@ int motor_FL_dir_Pin = 4;
 int FL_speed = 5;
 int motor_FR_dir_Pin = 2;
 int FR_speed = 3;
-int motor_BL_dir_Pin = 7;
-int BL_speed = 8;
-int motor_BR_dir_Pin = 9;
-int BR_speed = 10;
+int motor_BL_dir_Pin = 6;
+int BL_speed = 7;
+int motor_BR_dir_Pin = 8;
+int BR_speed = 9;
+
+
+int motor_FL_speed = 0;
+int motor_FR_speed = 0;
+int motor_BL_speed = 0;
+int motor_BR_speed = 0;
 
 int i = 0;
 
 void setup() {
   Serial.begin(9600);
 
-  for(i=0; i<12; i++){
+  for(i=2; i<10; i++){
     pinMode(i, OUTPUT);
   }
-  for(i=11; i<14; i++){
+  for(i=10; i<14; i++){
     pinMode(i, INPUT);
   }
 }
 
 void loop() {
-  int forward = map(pulseIn(11, HIGH, 25000), 1040, 1870, -254, 254);
-  int stripe = map(pulseIn(12, HIGH, 25000), 1115, 1780, -254, 254);
-  int yaw = map(pulseIn(13, HIGH, 25000), 998, 1905, -254, 254);
+  int forward_raw = constrain(pulseIn(12, HIGH), 1044, 1885);
+  int stripe_raw = constrain(pulseIn(13, HIGH), 1135, 1800);
+  int yaw_raw = constrain(pulseIn(11, HIGH, 25000), 1011, 1850);
+
+  int forward = map(forward_raw, 1044, 1885, -254, 254);
+  int stripe = map(stripe_raw, 1135, 1800, -254, 254);
+  int yaw = map(yaw_raw, 1011, 1850, -254, 254);
 
   //Serial.print(forward);
-  if(abs(forward) < 10){
+  if(abs(forward) < 24){
     forward = 0;
   }
   //Serial.println(forward);
-  if(abs(stripe) < 10){
+  if(abs(stripe) < 24){
     stripe = 0;
   }
-  if(abs(yaw) < 10){
+  if(abs(yaw) < 30){
     yaw = 0;
   }
 
-  int motor_FL_speed = map(forward - stripe - yaw, -254*3, 254*3, -254, 254);
-  int motor_FR_speed = map(forward + stripe + yaw, -254*3, 254*3, -254, 254);
-  int motor_BL_speed = map(forward + stripe - yaw, -254*3, 254*3, -254, 254);
-  int motor_BR_speed = map(forward - stripe + yaw, -254*3, 254*3, -254, 254);
+  /* 
+  Serial.print(forward);
+  Serial.print(", ");
+  Serial.print(stripe);
+  Serial.print(", ");
+  Serial.print(yaw);
+  Serial.println(", ");
+  */ 
+  
+  //Serial.print(stripe);
+  //Serial.print(", ");
+  
+  //Serial.println(forward_raw);
+  //Serial.print(", ");
+  
+
+  motor_FL_speed = map(forward - stripe + yaw, -254*3, 254*3, -254, 254);
+  motor_FR_speed = map(forward + stripe - yaw, -254*3, 254*3, -254, 254);
+  motor_BL_speed = map(forward + stripe + yaw, -254*3, 254*3, -254, 254);
+  motor_BR_speed = map(forward - stripe - yaw, -254*3, 254*3, -254, 254);
+
+   
+  Serial.print(motor_FL_speed);
+  Serial.print(", ");
+  Serial.print(motor_FR_speed);
+  Serial.print(", ");
+  Serial.print(motor_BL_speed);
+  Serial.print(", ");
+  Serial.println(motor_BR_speed);
+   
+  
+  int motor_FL_dir = (forward - stripe + yaw >= 0) ? HIGH : LOW;
+  int motor_FR_dir = (forward + stripe - yaw >= 0) ? HIGH : LOW;
+  int motor_BL_dir = (forward + stripe + yaw >= 0) ? LOW : HIGH;
+  int motor_BR_dir = (forward - stripe - yaw >= 0) ? LOW : HIGH;
 
   /*
-  Serial.print("motor FL ");
-  Serial.print(motor_FL_speed);
-  Serial.print(", motor FR ");
-  Serial.print(motor_FR_speed);
-  Serial.print(", motor BL ");
-  Serial.print(motor_BL_speed);
-  Serial.print(", motor BR ");
-  Serial.println(motor_BR_speed);
+  Serial.print(motor_FL_dir);
+  Serial.print(", ");
+  Serial.print(motor_FR_dir);
+  Serial.print(", ");
+  Serial.print(motor_BL_dir);
+  Serial.print(", ");
+  Serial.println(motor_BR_dir);
   */
 
   analogWrite(FL_speed, abs(motor_FL_speed));
   analogWrite(FR_speed, abs(motor_FR_speed));
   analogWrite(BL_speed, abs(motor_BL_speed));
   analogWrite(BR_speed, abs(motor_BR_speed));
-
-  int motor_FL_dir = (forward - stripe - yaw >= 0) ? HIGH : LOW;
-  int motor_FR_dir = (forward + stripe + yaw >= 0) ? HIGH : LOW;
-  int motor_BL_dir = (forward + stripe - yaw >= 0) ? HIGH : LOW;
-  int motor_BR_dir = (forward - stripe + yaw >= 0) ? HIGH : LOW;
 
   digitalWrite(motor_FL_dir_Pin, motor_FL_dir);
   digitalWrite(motor_FR_dir_Pin, motor_FR_dir);
