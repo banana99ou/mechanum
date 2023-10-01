ec1908d4468b38254e8347d11bc26c2833b96c5b
banana <banana99ou@gmail.com>
2023-10-01 19:26:55 +0900

    add move func

--- movement testing/move/move.ino
+++ movement testing/move/move.ino
@@ -1,7 +1,7 @@
 #define motor_FL_dir_Pin 4
-#define motor_FL_pwm_Pin 5
+#define motor_FL_pwm_Pin 3
 #define motor_FR_dir_Pin 2
-#define motor_FR_pwm_Pin 3
+#define motor_FR_pwm_Pin 5
 #define motor_BL_dir_Pin 6
 #define motor_BL_pwm_Pin 7
 #define motor_BR_dir_Pin 8 
@@ -45,7 +45,7 @@ void turn(int speed){
     rotate in int speed
     ccw -255 < speed < 255 cw
     */
-   if(speed<0){ // turn left when speed is smaller than 0
+    if(speed<0){ // turn left when speed is smaller than 0
         digitalWrite(motor_FL_dir_Pin, HIGH);  // left side go backward
         digitalWrite(motor_FR_dir_Pin, LOW); // right side go forward
         digitalWrite(motor_BL_dir_Pin, LOW);
@@ -69,6 +69,87 @@ void turn(int speed){
     analogWrite(motor_BR_pwm_Pin, abs(speed));
 }
 
+void move(int speed, int turn_radius){
+    /*
+    turn relative to point
+    turn_radius = distance to turn point from center of robot
+    if turn_radius > 0 turn point is in right side
+    else turn point is in left side
+
+    when turning point is in center of robot
+    robot need to turn in place
+    ->
+    check if turning_radius is smaller than robot's width
+
+    if turn_radius -> infinity then go forward
+    */
+    int wheel_width = 392;
+    int rightside_speed = 0;
+    int leftside_speed = 0;
+    if(abs(turn_radius)>wheel_width){
+        // turning point is outside of robot -> all wheels go forward
+        if(speed<0){ // set dir pin low when speed is smaller than 0
+            digitalWrite(motor_FL_dir_Pin, LOW);
+            digitalWrite(motor_FR_dir_Pin, LOW);
+            digitalWrite(motor_BL_dir_Pin, HIGH);
+            digitalWrite(motor_BR_dir_Pin, HIGH);
+        }
+        else if(speed>0){
+            digitalWrite(motor_FL_dir_Pin, HIGH);
+            digitalWrite(motor_FR_dir_Pin, HIGH);
+            digitalWrite(motor_BL_dir_Pin, LOW);
+            digitalWrite(motor_BR_dir_Pin, LOW);
+        }
+    }
+    else if(abs(turn_radius)<wheel_width){
+        // turning point is inside of robot -> 
+        if(speed<0){ // turn left when speed is smaller than 0
+            digitalWrite(motor_FL_dir_Pin, HIGH);  // left side go backward
+            digitalWrite(motor_FR_dir_Pin, LOW); // right side go forward
+            digitalWrite(motor_BL_dir_Pin, LOW);
+            digitalWrite(motor_BR_dir_Pin, HIGH);
+        }
+        else if(speed>0){
+            digitalWrite(motor_FL_dir_Pin, LOW);
+            digitalWrite(motor_FR_dir_Pin, HIGH);
+            digitalWrite(motor_BL_dir_Pin, HIGH);
+            digitalWrite(motor_BR_dir_Pin, LOW);
+        }
+    }
+    else{   // set motor speed to 0 when speed is 0
+        analogWrite(motor_FL_pwm_Pin, 0);
+        analogWrite(motor_FR_pwm_Pin, 0);
+        analogWrite(motor_BL_pwm_Pin, 0);
+        analogWrite(motor_BR_pwm_Pin, 0);
+    }
+    // Serial.println(turn_radius);
+    if(turn_radius > 0){
+        // Serial.println(">0");
+        // turning point to the right -> left side should turn faster
+        // rightside + leftside = speed 
+        // rightside/leftside = (2R+w)/(2R-w)
+        rightside_speed = 2*(abs(turn_radius)-wheel_width/2)*3.14*abs(speed)/(4*3.14*abs(turn_radius));
+        leftside_speed = 2*(abs(turn_radius)+wheel_width/2)*3.14*abs(speed)/(4*3.14*abs(turn_radius));
+    }
+    else if(turn_radius < 0){
+        // Serial.println("<0");
+        // turning point to the left -> right side should turn faster
+        rightside_speed = 2*(abs(turn_radius)+wheel_width/2)*3.14*abs(speed)/(4*3.14*abs(turn_radius));
+        leftside_speed = 2*(abs(turn_radius)-wheel_width/2)*3.14*abs(speed)/(4*3.14*abs(turn_radius));
+    }
+    else if(turn_radius == 0){
+        // turning point to the left -> right side should turn faster
+        rightside_speed = abs(speed);
+        leftside_speed = abs(speed);
+    }
+    // Serial.println(rightside_speed);
+    // Serial.println(leftside_speed);
+    analogWrite(motor_FL_pwm_Pin, leftside_speed);
+    analogWrite(motor_FR_pwm_Pin, rightside_speed);
+    analogWrite(motor_BL_pwm_Pin, leftside_speed);
+    analogWrite(motor_BR_pwm_Pin, rightside_speed);
+}
+
 void serialEvent() {
     while(Serial.available()){
         char inChar = (char)Serial.read();
@@ -89,6 +170,11 @@ void setup(){
 }
 
 void loop(){
+    // Serial.println("running");
+    move(100, 0);
+    delay(1000);
+    move(100, 1000);
+    delay(1000);
     serialEvent();
     if(stringComplete) {
 
@@ -102,6 +188,11 @@ void loop(){
             int amount = inputString.substring(5).toInt();
             turn(amount);
         }
+        // if(inputString.startsWith("move")) {
+        //     Serial.println("Ack: move");
+        //     int amount = inputString.substring(5).toInt();
+        //     move(amount);
+        // }
         
         inputString = "";
         stringComplete = false;
