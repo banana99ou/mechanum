cb70ffa2fdb199f05bb52ed194d2b852ebb3a763
banana <banana99ou@gmail.com>
2023-11-12 21:51:12 +0900

    add timeout

--- Sonar testing/Ping/Ping.ino
+++ Sonar testing/Ping/Ping.ino
@@ -25,16 +25,15 @@ void loop() {
 }
 
 int Ping(int echo, int trig) {
-  long duration;  //variable for the duration of sound wave travel
-  int distance;
-  digitalWrite(trig, LOW);
-  delayMicroseconds(1);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echo, HIGH);
-  // Calculating the distance
-  distance = duration * 0.34 / 2;  // Speed of sound wave divided by 2 (go and back)
-  return (distance);               //mm
+    long duration;  //variable for the duration of sound wave travel
+    int distance;
+    digitalWrite(trig, LOW);
+    delayMicroseconds(1);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echo, HIGH, 2000); // timeout of 2000 microseconds approx 0.68m
+    distance = duration * 0.34 / 2;  // Speed of sound wave divided by 2 (go and back)
+    return (distance);               //mm
 }
