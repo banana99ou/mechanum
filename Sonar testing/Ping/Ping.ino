fa4544eaf35197a37e020db34a5abebdeba6fa17
banana <banana99ou@gmail.com>
2023-11-15 22:25:35 +0900

    idk

--- Sonar testing/Ping/Ping.ino
+++ Sonar testing/Ping/Ping.ino
@@ -1,5 +1,5 @@
-#define TRIG_FF 11
-#define ECHO_FF 10
+#define TRIG_FF 10
+#define ECHO_FF 11
 
 void setup() {
   Serial.begin(9600);
@@ -10,18 +10,7 @@ void setup() {
 void loop() {
   int result1 = Ping(ECHO_FF, TRIG_FF);
   Serial.println(result1);
-  // long duration;  //variable for the duration of sound wave travel
-  // int distance;
-  // digitalWrite(trig, LOW);
-  // delayMicroseconds(1);
-  // digitalWrite(trig, HIGH);
-  // delayMicroseconds(10);
-  // digitalWrite(trig, LOW);
-  // // Reads the echoPin, returns the sound wave travel time in microseconds
-  // duration = pulseIn(echo, HIGH);
-  // // Calculating the distance
-  // distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
-  // Serial.println(distance);
+  result1 = 0.9248*result1 - 10.04;
 }
 
 int Ping(int echo, int trig) {
@@ -33,7 +22,7 @@ int Ping(int echo, int trig) {
     delayMicroseconds(10);
     digitalWrite(trig, LOW);
     // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echo, HIGH, 2000); // timeout of 2000 microseconds approx 0.68m
+    duration = pulseIn(echo, HIGH, 50000); // timeout of 50000 microseconds approx 8.5m
     distance = duration * 0.34 / 2;  // Speed of sound wave divided by 2 (go and back)
     return (distance);               //mm
 }
